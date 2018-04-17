a29d70946df23aa30d8b02ad6f8b2e88103dc56e
huzhaoheng <huzhaoheng9435@gmail.com>
2018-04-17 17:06:39 -0500

    17:06@04/17/2018

--- static/js/data-flow.js
+++ static/js/data-flow.js
@@ -1,5 +1,32 @@
+var nodes = null;
+var edges = null;
+var network = null;
+
+var repository_list = null;
+var dataset_list = null;
+
 $( "#data-flow" ).on('shown.bs.modal', function(){
 	init();
+
+	var query = loadDatasetListQuery();
+	$.getJSON(
+		'/getDatasetList',
+		{arg: JSON.stringify({"query" : query})},
+		function (response){
+			var result = response.elements;
+			dataset_list = result['datasetList'];
+		}
+	)
+
+	var query = loadRepositoryListQuery();
+	$.getJSON(
+		'/getRepositoryList',
+		{arg: JSON.stringify({"query" : query})},
+		function (response){
+			var result = response.elements;
+			repository_list = result['repositoryList'];
+		}
+	)
 })
 
 $("#data-flow-submit").on('click', function () {
@@ -16,13 +43,8 @@ $("#data-flow .close").on('click', function () {
 })
 
 
-var nodes = null;
-var edges = null;
-var network = null;
-
 // randomly create some nodes and edges
-/*var data = getScaleFreeNetwork(25);*/
-var data = {
+/*var data = {
 				'edges' : [
 					{from: 1, to: 0, id: "ce07698a-f1ba-4955-b8a2-ffd08ea7fb19"},
 					{from: 2, to: 1, id: "07b4a013-e906-42e0-8591-a73efbd79e57"}
@@ -32,21 +54,13 @@ var data = {
 					{id: 1, label: "1"},
 					{id: 2, label: "2"}
 				]
+		};*/
+
+var data = {
+				'edges' : [],
+				'nodes' : []
 		};
 
-var seed = 2;
-
-function setDefaultLocale() {
-	var defaultLocal = navigator.language;
-	var select = document.getElementById('locale');
-	select.selectedIndex = 0; // set fallback value
-	for (var i = 0, j = select.options.length; i < j; ++i) {
-		if (select.options[i].getAttribute('value') === defaultLocal) {
-			select.selectedIndex = i;
-			break;
-		}
-	}
-}
 
 function destroy() {
 	if (network !== null) {
@@ -63,15 +77,14 @@ function draw() {
 
 	var container = document.getElementById('data-flow-graph');
 	var options = {
-			layout: {randomSeed:seed}, // just to make sure the layout is the same when the locale is changed
-			/*locale: document.getElementById('locale').value,*/
+			layout: {
+				improvedLayout: true
+			},
 			manipulation: {
 				addNode: function (data, callback) {
 					$("#network-popUp").css('z-index', 9999);
 					// filling in the popup DOM elements
 					document.getElementById('operation').innerHTML = "Add Node";
-					document.getElementById('node-id').value = data.id;
-					document.getElementById('node-label').value = data.label;
 					document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
 					document.getElementById('cancelButton').onclick = clearPopUp.bind();
 					document.getElementById('network-popUp').style.display = 'block';
@@ -80,8 +93,6 @@ function draw() {
 					$("#network-popUp").css('z-index', 9999);
 					// filling in the popup DOM elements
 					document.getElementById('operation').innerHTML = "Edit Node";
-					document.getElementById('node-id').value = data.id;
-					document.getElementById('node-label').value = data.label;
 					document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
 					document.getElementById('cancelButton').onclick = cancelEdit.bind(this,callback);
 					document.getElementById('network-popUp').style.display = 'block';
@@ -96,7 +107,11 @@ function draw() {
 					else {
 						callback(data);
 					}
-				}
+				},
+
+				/*aaa: function(data, callback){
+					return;
+				}*/
 			}
 		};
 	network = new vis.Network(container, data, options);
@@ -107,6 +122,9 @@ function clearPopUp() {
 	document.getElementById('saveButton').onclick = null;
 	document.getElementById('cancelButton').onclick = null;
 	document.getElementById('network-popUp').style.display = 'none';
+
+	$("#data-flow-type").parent().find('.data-flow-type-btn').text("Type");
+	$("#data-flow-name").parent().find('.data-flow-name-btn').text("Name");
 }
 
 function cancelEdit(callback) {
@@ -115,13 +133,48 @@ function cancelEdit(callback) {
 }
 
 function saveData(data,callback) {
-	data.id = document.getElementById('node-id').value;
-	data.label = document.getElementById('node-label').value;
+	/*data.id = document.getElementById('node-id').value;
+	data.label = document.getElementById('node-label').value;*/
+	var type = $("#data-flow-type").parent().find('.data-flow-type-btn').text();
+	var name = $("#data-flow-name").parent().find('.data-flow-name-btn').text();
+
+	if (type == 'Type' || name == 'Name'){
+		window.alert("Please specify Type and Name");
+		return;
+	}
+
+	data.label = type + ":" + name;
 	clearPopUp();
 	callback(data);
 }
 
 function init() {
-	/*setDefaultLocale();*/
 	draw();
+}
+
+function dataFlowTypeLiClickHandler(ele){
+	var selected = $(ele).find('a')[0].innerText;
+	$(ele).parent().prev().text(selected);
+
+	$("#data-flow-name").parent().find('.data-flow-name-btn').text("Name");
+
+	if (selected == 'Repository'){
+		var code = "";
+		repository_list.forEach(repository => {
+			code += "<li onclick='dataFlowNameLiClickHandler(this);'><a href='#'>" + repository + "</a></li>"
+		})
+		$("#data-flow-name").html(code);
+	}
+	else{
+		var code = "";
+		dataset_list.forEach(dataset => {
+			code += "<li onclick='dataFlowNameLiClickHandler(this);'><a href='#'>" + dataset + "</a></li>"
+		})
+		$("#data-flow-name").html(code);
+	}
+}
+
+function dataFlowNameLiClickHandler(ele){
+	var selected = $(ele).find('a')[0].innerText;
+	$(ele).parent().prev().text(selected);
 }
