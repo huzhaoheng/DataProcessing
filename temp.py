54771111c7769b733bac4ff2470d5643d0a85a81
huzhaoheng <huzhaoheng9435@gmail.com>
2018-04-23 04:15:23 -0500

    04:12@04/23/2018

--- temp.py
+++ temp.py
@@ -23,9 +23,8 @@ def applyStatisticalFunction(data, function, values, name):
     elif function == 'MIN':
         return {name : min(data)}
     elif function == 'AVG':
-        print (data)
-        return {name : np.average(data)}
-        # return {name : np.nanmean(data)}
+        # return {name : np.average(data)}
+        return {name : np.nanmean(data)}
         # return sum(data) / len(data)
     elif function == 'SUM':
         return {name : sum(data)}
