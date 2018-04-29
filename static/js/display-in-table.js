1a55e835327b70b33c13bcc92acc44aa62edd5e0
huzhaoheng <huzhaoheng9435@gmail.com>
2018-04-28 23:59:43 -0500

    23:59@04/28/2018

--- static/js/display-in-table.js
+++ static/js/display-in-table.js
@@ -184,7 +184,9 @@ function displayInTable(result, type, container = '#table') {
 		for (var i = 0; i < result.length; i++) {
 			formatted_data.push(formatData(result[i]["data"]));
 		}
+		console.log(formatted_data);
 		var temp = pivot(formatted_data);
+		console.log(temp);
 		// var columns = [{checkbox: true, visible: true}];
 		var columns = [];
 		temp[0].forEach(n => {
