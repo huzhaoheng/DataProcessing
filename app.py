cd7cec45fd41a40350a314d17fdeb0a10366348b
huzhaoheng <huzhaoheng9435@gmail.com>
2018-04-28 19:59:48 -0500

    19:59@04/28/2018

--- app.py
+++ app.py
@@ -75,11 +75,8 @@ def home():
 @app.route('/getUpdateTime')
 def getUpdateTime():
     query = json.loads(request.args.get('arg'))['query']
-    print (query)
     result = graph.cypher.execute(query)
-    print (result)
     result = pandas.DataFrame(result.records, columns=result.columns).values.tolist()[0][0]
-    print (result)
     return jsonify(elements = {"update_time" : result})
 
 @app.route('/getRepositoryList')
